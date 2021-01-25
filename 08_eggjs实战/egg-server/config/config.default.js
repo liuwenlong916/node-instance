8f87b32bf0a950bd9192e89693c3d597e8ff02bd
liuwenlong916 <1141657011@qq.com>
2021-01-25 17:25:55 +0800

    错误统一应答方式二:config.onerror

--- 08_eggjs实战/egg-server/config/config.default.js
+++ 08_eggjs实战/egg-server/config/config.default.js
@@ -16,7 +16,8 @@ module.exports = appInfo => {
   config.keys = appInfo.name + "_1611542775684_1001";
 
   // add your middleware config here
-  config.middleware = ["errorHandler"];
+  // config.middleware = ["errorHandler"];
+  config.middleware = [];
 
   // add your user config here
   const userConfig = {
@@ -37,6 +38,29 @@ module.exports = appInfo => {
     routerMap: true, //自动加载路由，更新文件router.js
     enable: true,
   };
+  //错误统一处理方法二
+  config.onerror = {
+    all(err, ctx) {
+      // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
+      ctx.app.emit("error", err, this);
+      const status = err.status || 500;
+      // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
+      const error =
+        status === 500 && ctx.app.config.env === "prod"
+          ? "Internal Server Error"
+          : err.message;
+      // 从 error 对象上读出各个属性，设置到响应中
+      ctx.body = {
+        code: status, // 服务端自身的处理逻辑错误(包含框架错误500 及 自定义业务逻辑错误533开始 ) 客户端请求参数导致的错误(4xx开始)，设置不同的状态码
+        error: error,
+      };
+      if (status === 422) {
+        ctx.body.detail = err.errors;
+      }
+      ctx.status = 200;
+    },
+  };
+
   return {
     ...config,
     ...userConfig,
